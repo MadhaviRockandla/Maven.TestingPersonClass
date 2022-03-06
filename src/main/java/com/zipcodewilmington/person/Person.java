33a63086c9f982f6857dd2d0e6cbb1c8393c9f56
MadhaviRockandla <rokandla@gmail.com>
2022-03-06 17:03:54 -0500

    Added 5 more fields to Person class

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -33,4 +33,42 @@ public class Person {
     public Integer getAge() {
         return this.age;
     }
+    public void setlastName(String lastname) {
+        this.lastName = lastname;
+    }
+
+    public void setmiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setBirth(String Birth) {
+        this.Birthdate = Birth;
+    }
+
+    public void setheightInInches(int height) {
+        this.heightInInches = height;
+    }
+
+    public void setweight(int weight) {
+        this.weight = weight;
+    }
+    public String getlastName() {
+        return lastName;
+    }
+
+    public String getmiddleName() {
+        return middleName;
+    }
+
+    public String getBirth() {
+        return Birthdate;
+    }
+
+    public int getheightInInches() {
+        return heightInInches;
+    }
+
+    public int getweight() {
+        return weight;
+    }
 }
